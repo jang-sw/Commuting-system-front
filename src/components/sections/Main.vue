67a6faaa89ef56afa62a881a1e549514810697e3
jang-sw <jsw118536@gmail.com>
2024-07-08 17:41:54 +0900

    메인페이지 api 연동 1차 완료

--- src/components/sections/Main.vue
+++ src/components/sections/Main.vue
@@ -1,10 +1,11 @@
 <template>
     <main>
         <div class="dashboard">
-            <div class="actions">
-                <button class="clock-in">업무 시작</button>
-                <button class="clock-out">업무 종료</button>
-                <button class="ooo">외출(자리 비움)</button>
+            <div class="actions" v-if="state == '업무전' || state == '업무중' || state == '외출중' || state == '업무 종료'">
+                <button v-bind:disabled="processing" class="clock-in" v-if="state == '업무전'" @click="changeCommutingState('START')">업무 시작</button>
+                <button v-bind:disabled="processing" class="clock-out" v-if="state == '업무중'" @click="changeCommutingState('END')">업무 종료</button>
+                <button v-bind:disabled="processing" class="ooo" v-if="state == '업무중'" @click="changeCommutingState('OUTING')">외출(자리 비움)</button>
+                <button v-bind:disabled="processing" class="clock-in" v-if="state == '외출중'" @click="changeCommutingState('RESTART')">외출 복귀</button>
             </div>
             <div class="records">
                 <h3>현재 상태 : <span style="color: #685e57;">{{ state }}</span></h3>
@@ -14,10 +15,10 @@
             <h4 style="margin: 0; color: #514e4c;">출퇴근 인원 검색</h4>
             <hr style="background:#867e78; height:1px; border:0; margin-top: 0; ">
             <div class="search">
-                <input type="text"/>
-                <button class="search-btn"> 검색 </button>
+                <input type="text" v-model="keyword"/>
+                <button v-bind:disabled="processing" class="search-btn" @click="search()"> 검색 </button>
             </div>
-            <div class="search-result">
+            <div class="search-result" v-html="list">
 
             </div>
         </div>
@@ -37,10 +38,16 @@
         data(){
             return { 
                 serverUrl: conf.server,
-                state:""
+                state:"",
+                keyword:'',
+                processing: false,
+                list: ''
             }
         },
         async created() {
+            this.keyword='';
+            this.list = '';
+            this.processing= false
             const chkRes = await chkSession(this.serverUrl)
             if(chkRes != 1) this.$router.push("/login");
             this.getTodayCommuting ();
@@ -70,11 +77,111 @@
                     }
                     if(!this.state)this.state = '업무전'
                     else this.state =  this.state == 'START' ? '업무중' : this.state == 'END' ? '업무 종료' : this.state == 'OUTING' ? '외출중' : this.state
+               
                 }).catch( async (err) =>{
                     console.log(err)
                 })
+            },
+            async changeCommutingState(commutingState: string){
+                this.processing = true;
+                const token = sessionStorage.getItem('token')
+                const userInfo = sessionStorage.getItem('userInfo')
+                const parsedUserInfo: any = qs.parse(userInfo ?? '');
+                const reqestPath = this.serverUrl + (commutingState == 'START' ? '/api/commute/clockIn' : commutingState == 'END' ? '/api/commute/clockOut' : commutingState == 'OUTING' ? '/api/commute/outing' : '/api/commute/restart')
+                let confirm = await swal.fire({
+                    text: commutingState == 'START' ? '업무를 시작합니다.' : commutingState == 'END' ? '업무를 종료합니다.' : commutingState == 'OUTING' ? '외출합니다.' : '외출에서 복귀합니다.',
+                    icon: 'success',
+                    confirmButtonText : "OK",
+                    cancelButtonText : "NO",
+                    showCancelButton : true,
+                })
+                if(!confirm.isConfirmed) {
+                    this.processing = false;
+                    return;
+                }
+                return await axios.post(
+                    reqestPath,"",
+                    {
+                        headers: { 
+                            'content-type': 'application/x-www-form-urlencoded' 
+                            , 'accountId' : parsedUserInfo['accountId']
+                            , 'Authorization': token
+                        }
+                    }
+                ).then( async (res)=>{
+                    if(res.data.result == 1){
+                        this.processing = false;
+                        this.state = commutingState == 'START' ? '업무중' : commutingState == 'END' ? '업무종료' : commutingState == 'OUTING' ? '외출중' : '업무중'
+                       
+                    }else {
+                        this.processing = false;
+                        await swal.fire({
+                            text:'에러가 발생했습니다. 다시 시도해 주세요',
+                            icon: 'error'
+                        });
+                        location.reload()
+                    }
+                }).catch( async (err) =>{
+                    this.processing = false;
+                    await swal.fire({
+                            text:'에러가 발생했습니다. 다시 시도해 주세요',
+                            icon: 'error'
+                        });
+                    console.log(err)
+                    location.reload()
+                })
+            },
+            async search(){
+                const token = sessionStorage.getItem('token')
+                const userInfo = sessionStorage.getItem('userInfo')
+                const parsedUserInfo: any = qs.parse(userInfo ?? '');
+                this.processing = true;
+                return await axios.get(
+                    `${this.serverUrl}/api/commute/todayByName`,
+                    {
+                        headers: { 
+                            'content-type': 'application/x-www-form-urlencoded' 
+                            , 'accountId' : parsedUserInfo['accountId']
+                            , 'Authorization': token
+                        },
+                        params:{
+                            'name': this.keyword
+                        }
+                    }
+                ).then( async (res)=>{
+                    if(res.data.result == 1){
+                        let html = '';
+                        let list: { 
+                            email: string,
+                            name: string,
+                            team: string,
+                            position: string
+                        }[] = res.data.data
+                        list.map(ele => {
+                            html += `${ele.name} (${ele.team}, ${ele.position}, ${ele.email}) <br/>`
+                        });
+                        this.list = html;
+                    }else{
+                        await swal.fire({
+                            text:'에러가 발생했습니다. 다시 시도해 주세요',
+                            icon: 'error'
+                        });
+                        
+                    }
+                    this.processing = false;
+                    location.reload()
+                }).catch( async (err) =>{
+                    await swal.fire({
+                        text:'에러가 발생했습니다. 다시 시도해 주세요',
+                        icon: 'error'
+                    });
+                    console.log(err)
+                    this.processing = false;
+                    location.reload()
+                })
             }
         },
+        
     });
 </script>
 <style>
