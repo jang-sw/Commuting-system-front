89d3e14d9c65f90fa4c7343d3fc5729e3d32027e
jang-sw <jsw118536@gmail.com>
2024-07-10 16:29:37 +0900

    신규 이용자 등록 추가

--- src/components/sections/admin/AdminUser.vue
+++ src/components/sections/admin/AdminUser.vue
@@ -5,8 +5,8 @@
             <div class="search">
                 <input placeholder="이름으로 검색" v-model="name" >
                 <button  class="search-btn" @click="getUser(0)"> 검색 </button>
-            </div>
-            
+            </div><br/>
+            <button @click="newUserModalOpen()">신규 등록</button>
             <hr/>
             <table>
                 <thead>
@@ -35,7 +35,7 @@
             <div class="modal-background" id="modal-background" ></div>
             <div class="request-container" id="user-modal">
                 <form id="userequestForm" class="request-form" @submit="updateUser($event, 0)">
-                    <h2>Leave Request <span style="float: right; color:darkgray; cursor: pointer;" @click="userModalCloase()">X</span></h2>
+                    <h2>User Update <span style="float: right; color:darkgray; cursor: pointer;" @click="userModalCloase()">X</span></h2>
                     <div class="input-group">
                         <label for="name">이름</label>
                         <input type="text" id="name" name="name" required v-model="selected.name" >
@@ -55,6 +55,32 @@
                     <button type="submit" v-bind:disabled="isProcessing">정보 수정</button>
                     <button type="button" style="margin-bottom: 10px; margin-top: 10px;" v-bind:disabled="isProcessing" @click="resetPwd(0)">비밀번호 초기화</button>
                     <button type="button" class="delete-btn"  v-bind:disabled="isProcessing" @click="deleteUser(0)">삭제</button>
+
+                    <div id="success-message"></div>
+                </form>
+            </div>
+
+            <div class="request-container" id="new-user-modal">
+                <form id="userequestForm" class="request-form" @submit="createUser($event, 0)">
+                    <h2>Create User <span style="float: right; color:darkgray; cursor: pointer;" @click="newUserModalCloase()">X</span></h2>
+                    <div class="input-group">
+                        <label for="name">이름</label>
+                        <input type="text" id="name" name="name" required v-model="newUser.name">
+                    </div>
+                    <div class="input-group">
+                        <label for="team">팀</label>
+                        <input type="text" id="team" name="team" required v-model="newUser.team">
+                    </div>
+                    <div class="input-group">
+                        <label for="position">직책</label>
+                        <input type="text" id="position" name="position" required v-model="newUser.position" >
+                    </div>
+                    <div class="input-group">
+                        <label for="email">이메일</label>
+                        <input type="text" id="email" name="email" required v-model="newUser.email">
+                    </div>
+                    ※초기 비밀번호는 1234
+                    <button type="submit" v-bind:disabled="isProcessing">등록</button>
 
                     <div id="success-message"></div>
                 </form>
@@ -96,6 +122,12 @@
                     team: '',
                     position: '',
                     email: '',
+                },
+                newUser: {
+                    name: '',
+                    team: '',
+                    position: '',
+                    email: '',
                 }
             }
         }, 
@@ -119,13 +151,32 @@
 
             },
             userModalCloase(){
-                const modal = document.getElementById('user-modal');
+                const modal = document.getElementById('new-user-modal');
+                const background = document.getElementById('modal-background');
+                modal!.style.display = 'none';
+                background!.style.display = 'none';
+
+                this.newUser = {
+                    name: '',
+                    team: '',
+                    position: '',
+                    email: '',
+                }
+            },
+            newUserModalOpen(){
+                const modal = document.getElementById('new-user-modal');
+                const background = document.getElementById('modal-background');
+                modal!.style.display = 'block';
+                background!.style.display = 'block';
+
+            },
+            newUserModalCloase(){
+                const modal = document.getElementById('new-user-modal');
                 const background = document.getElementById('modal-background');
                 modal!.style.display = 'none';
                 background!.style.display = 'none';
             },
             async updateUser(event:Event, retry: number){
-                console.log(12)
                 this.isProcessing = true;
                 event.preventDefault();
                 const token = sessionStorage.getItem('token')
@@ -180,6 +231,71 @@
                     if(err?.response?.status == 401 && retry != 1 ){
                         if(await refreshSession(this.serverUrl) == 1){
                             await this.updateUser(event, 1);
+                            return;
+                        }
+                    }
+                    await swal.fire({
+                        text:'에러가 발생했습니다. 다시 시도해 주세요',
+                        icon: 'error'
+                    });
+                    this.$router.push("/login");
+                });
+            },
+            async createUser(event:Event, retry: number){
+                this.isProcessing = true;
+                event.preventDefault();
+                const token = sessionStorage.getItem('token')
+                const userInfo = sessionStorage.getItem('userInfo')
+                const parsedUserInfo: any = qs.parse(userInfo ?? '');
+                let confirm = await swal.fire({
+                    icon: 'question',
+                    text: '신규로 이용자를 등록합니까?',
+                    confirmButtonText : "OK",
+                    cancelButtonText : "NO",
+                    showCancelButton : true,
+                })
+                if(!confirm.isConfirmed){
+                    this.isProcessing = false;
+                    return;
+                }
+                return await axios.post(
+                    `${this.serverUrl}/adminApi/account/create`,
+                    qs.stringify(this.newUser),
+                    {
+                        headers: { 
+                            'content-type': 'application/x-www-form-urlencoded' 
+                            , 'accountId' : parsedUserInfo['accountId']
+                            , 'Authorization': token
+                        }
+                    }
+                ).then(res => {
+                    if(res.data.result == 1){
+                        swal.fire({
+                            text:'성공적으로 유저를 등록 했습니다.',
+                            icon: 'success'
+                        });
+                        this.getUser(0);
+                        this.userModalCloase();
+                    }else if(res.data.result == -2){
+                        swal.fire({
+                            text:'이미 등록된 이메일 입니다.',
+                            icon: 'warning'
+                        });
+                    }else {
+                        swal.fire({
+                            text:'에러가 발생했습니다. 다시 시도해 주세요',
+                            icon: 'error'
+                        });
+                    }
+
+                    this.isProcessing = false;
+                    return;
+                }).catch(async err => {
+                    console.log(err)
+                    this.isProcessing = false;
+                    if(err?.response?.status == 401 && retry != 1 ){
+                        if(await refreshSession(this.serverUrl) == 1){
+                            await this.createUser(event, 1);
                             return;
                         }
                     }
